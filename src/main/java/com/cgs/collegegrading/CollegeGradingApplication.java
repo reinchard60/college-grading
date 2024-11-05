780c55e6624a88c67900f3c75a905a55c3402331
Reinchard Lang Velez <reinchard.lang@ksp.com.mx>
2024-11-05 13:50:22 -0500

    Added base spring configuration

---
+++ src/main/java/com/cgs/collegegrading/CollegeGradingApplication.java
@@ -0,0 +1,13 @@
+package com.cgs.collegegrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeGradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeGradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cgs/collegegrading/CollegeGradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgs.collegegrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeGradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
